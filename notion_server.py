b8803efecd5c65bd097dd2048a7edd40a99c6509
대전1반 권기범 <qja1998@naver.com>
2025-04-17 17:58:37 +0900

    :sparkle: Feat: notion mcp 관련 코드 추가

--- notion_server.py
+++ notion_server.py
@@ -13,27 +13,20 @@ OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 from agents import Agent, Runner, trace
 from agents.mcp import MCPServer, MCPServerStdio
 
-async def run(mcp_server: MCPServer, directory_path: str):
+async def run(mcp_server: MCPServer):
     agent = Agent(
         model=OPENAI_MODEL,
         name="Assistant",
-        instructions=f"Answer questions about the git repositories, use that for repo_path",
+        instructions=f"Answer questions about the notion documents",
         mcp_servers=[mcp_server],
     )
-    
-    user_id = directory_path.split("/")[-1]
-
-    repositories = await Runner.run(starting_agent=agent, input=f"{directory_path}의 모든 repository를 '\n'로 구분해서 나열")
 
     PROMPT = f"""
-    You are a potfolio assistant. You are given {directory_path}.
-    철저하게 {user_id} 관점에서 답변하라.
-    You should answer questions about the git repository.
-    NEVER answer questions about the directories that are not in the git repository.
-    GET 호출을 할 때는 반드시 실제 있는 디렉토리를 사용해야 한다.
+    You are a potfolio assistant. You are given the user's notion page.
+    철저하게 사용자 관점에서 답변하라.
+    You should answer questions about the notion documents.
+    NEVER answer questions about the directories that are not in the notion documents.
     답변은 절대 지어내지 말고, 항상 사실에 기반해야 한다.
-    You should answer questions about the git repository.
-    repositories: {repositories.final_output}
     """
 
     chat_history = deque([], maxlen=5)
@@ -65,27 +58,17 @@ async def run(mcp_server: MCPServer, directory_path: str):
 
 async def main():
     # Ask the user for the directory path
-    directory_path = "https://github.com/qja1998"
 
     async with MCPServerStdio(
         cache_tools_list=True,  # Cache the tools list, for demonstration
         params={
-            "command": "docker",
-            "args": [
-                "run",
-                "-i",
-                "--rm",
-                "-e",
-                "GITHUB_PERSONAL_ACCESS_TOKEN",
-                "ghcr.io/github/github-mcp-server"
-            ],
-            "env": {
-                "GITHUB_PERSONAL_ACCESS_TOKEN": GITHUB_PERSONAL_ACCESS_TOKEN,
-            }
-        },
+            "command": "python",
+            "args": ["-m", "notion_mcp"],
+        }
     ) as server:
-        with trace(workflow_name="MCP Git Example"):
-            await run(server, directory_path)
+        print("setted notion server")
+        with trace(workflow_name="MCP Notion Example"):
+            await run(server)
 
 
 if __name__ == "__main__":
